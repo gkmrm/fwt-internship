fb22275e3888ae10c121e5decb51ec01e784cbf2
Gleb <gkmrm98@gmail.com>
2023-06-02 16:50:06 +0400

    create assets + add svg's + export

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,25 @@
+import arrowL from './arrowL.svg';
+import arrowR from './arrowR.svg';
+import dashForRange from './dashForRange.svg';
+import dashForRangeDark from './dashForRangeDark.svg';
+import doubleArrowL from './doubleArrowL.svg';
+import doubleArrowR from './doubleArrowR.svg';
+import FWTLogo from './FWTLogo.svg';
+import iconDarkTheme from './iconDarkTheme.svg';
+import iconLightTheme from './iconLightTheme.svg';
+import resetButton from './resetButton.svg';
+import selectArrow from './selectArrow.svg';
+
+export {
+  iconDarkTheme,
+  iconLightTheme,
+  FWTLogo,
+  arrowL,
+  arrowR,
+  doubleArrowL,
+  doubleArrowR,
+  resetButton,
+  selectArrow,
+  dashForRange,
+  dashForRangeDark,
+};
